29dd58562aca89b21cdbeda2f45a94f714060ece
Christoph Hahn <chrhhn@gmail.com>
2023-03-14 13:20:02 +0100

    Updated texture filter to TEXTURE_FILTER_POINT - means no filter, only pixel aproximation.

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main() {
     // Render texture initialization, used to hold the rendering result so we can easily resize it
     RenderTexture2D target = LoadRenderTexture(Game::ScreenWidth, Game::ScreenHeight);
     // Texture scale filter to use
-    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);
 
 #ifdef GAME_START_FULLSCREEN
     ToggleFullscreen();
